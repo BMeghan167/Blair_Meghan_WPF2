283e6c7005e2cc802b882600b3fb8068d53cbb9c
BMeghan167 <mlblair@fullsail.edu>
2015-04-09 20:21:13 -0400

    Expressions Assignment
    April 9, 2015
    Section 2

---
+++ Blair_Meghan_Expressions_Assignment/JS/script.js
@@ -0,0 +1,14 @@
+//Meghan Blair
+//April 9, 2015
+//Expressions Assignment
+
+fabricChoice = [4.98, 5.99, 8.73]
+
+chiffonYards = prompt("How many yards of Chiffon do you want?");
+cottonYards = prompt("How many yards of Cotton do you want?");
+satinYards = prompt("How many yards of Satin do you want?");
+
+chiffonTotal = fabricChoice[0] * chiffonYards
+cottonTotal = fabricChoice[1] * cottonYards
+satinTotal = fabricChoice[2] * satinYards
+
