598d54a70ff26c5695a7882856d020ac0a55b513
BMeghan167 <mlblair@fullsail.edu>
2015-04-16 14:06:52 -0400

    Conditionals Assignment
    April 16, 2015

---
+++ Blair_Meghan_Conditionals_Assignment/JS/script.js
@@ -0,0 +1,13 @@
+//Meghan Blair
+//April 16, 2015
+//Conditionals Assignment
+
+var songCost = 1.27
+var albumCost = 9.99
+var appCost = 2.99
+var moneyLeft = 30
+
+var songAmount = prompt("How many songs would you like to purchase from iTunes?");
+var albumAmount = prompt("How many albums would you like to purchase from iTunes?");
+var appAmount = prompt("How many albums would you like to purchase from the App store?");
+
