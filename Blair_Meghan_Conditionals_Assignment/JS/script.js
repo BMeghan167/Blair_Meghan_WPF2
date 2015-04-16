e91c77e9a6329565cf346143170d7fa9863dde29
BMeghan167 <mlblair@fullsail.edu>
2015-04-16 14:11:22 -0400

    Conditionals Assignment 2
    April 16, 2015

--- Blair_Meghan_Conditionals_Assignment/JS/script.js
+++ Blair_Meghan_Conditionals_Assignment/JS/script.js
@@ -11,3 +11,20 @@ var songAmount = prompt("How many songs would you like to purchase from iTunes?"
 var albumAmount = prompt("How many albums would you like to purchase from iTunes?");
 var appAmount = prompt("How many albums would you like to purchase from the App store?");
 
+if (songAmount === ""){
+    prompt("You didn't tell me how many songs you wanted to buy!");
+}else{
+    console.log("Good job, you already told me how many songs you want!");
+}
+
+if (albumAmount === ""){
+    prompt("You forgot to tell me how many albums you want to purchase!");
+}else{
+    console.log("Great! I have the amount of albums you want to buy!");
+}
+
+if (appAmount === ""){
+    prompt("Hold up! You didn't tell me how many apps you want to get!");
+}else{
+    console.log("Yay! I have the amount of apps you want to purchase!");
+}
