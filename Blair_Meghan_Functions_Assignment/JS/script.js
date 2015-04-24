5b64d1d1ea6c46ee946055b0253ead07ed99ab69
BMeghan167 <mlblair@fullsail.edu>
2015-04-23 21:40:10 -0400

    Functions Assignment 2
    April 23, 2015

--- Blair_Meghan_Functions_Assignment/JS/script.js
+++ Blair_Meghan_Functions_Assignment/JS/script.js
@@ -5,7 +5,17 @@
 var bdayCake = prompt("How much are you spending on the birthday cake?");
 var decorCost = prompt("How much are you spending on the decorations?");
 var foodCost = prompt("How much did you spend on food?");
+var pizzaNum = prompt("How many pizzas are you getting for the party?");
 var peopleAmount = prompt("How many people are coming again?");
 
 /*First we'll figure out how much it will cost for the three things the user is first prompted for*/
+
+var total = supplyCost(bdayCake, decorCost, foodCost);
+
+function supplyCost(a, b, c){
+    var add = a + b + c;
+    return add;
+}
+
+console.log("The total amount you are spending on the birthday cake, decorations and food is " + total + " .");
 
