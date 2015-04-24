823302a76454e8970eb2bc699190770f38cf317b
BMeghan167 <mlblair@fullsail.edu>
2015-04-23 21:21:56 -0400

    Functions Assignment
    April 23, 2015

---
+++ Blair_Meghan_Functions_Assignment/JS/script.js
@@ -0,0 +1,11 @@
+//Meghan Blair
+//April 23, 2015
+//Functions Assignment
+
+var bdayCake = prompt("How much are you spending on the birthday cake?");
+var decorCost = prompt("How much are you spending on the decorations?");
+var foodCost = prompt("How much did you spend on food?");
+var peopleAmount = prompt("How many people are coming again?");
+
+/*First we'll figure out how much it will cost for the three things the user is first prompted for*/
+
